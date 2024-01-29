8602ec2cc34423fd638c9b073b36f9cb4a579d9a
ispervoykin <ilya.perv2@gmail.com>
2024-01-29 14:23:56 +0300

    Add multiple Dockers

--- src/database.py
+++ src/database.py
@@ -1,13 +1,11 @@
 from sqlalchemy import create_engine
-from sqlalchemy_utils import database_exists, create_database, drop_database
+from sqlalchemy_utils import database_exists, create_database
 from sqlalchemy.orm import sessionmaker, declarative_base
 from config import DB_NAME, DB_PASS, DB_HOST, DB_PORT, DB_USER
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
-if database_exists(engine.url):
-    drop_database(engine.url)
 if not database_exists(engine.url):
     create_database(engine.url)
 
